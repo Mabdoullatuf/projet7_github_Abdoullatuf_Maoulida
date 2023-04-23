3ef0743ff4758e9c83e4c3c71f2cd6f02ddc402e
Mabdoullatuf <a.ellatuf@gmail.com>
2023-04-23 13:32:41 +0200

    mise à jour de test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,30 @@
+import joblib
+import pytest
+import pandas as pd
 import pytest
 import requests
 from fichier_streamlit import get_prediction
+
+@pytest.fixture(scope='module')
+def load_model():
+    model = joblib.load('LGBM_best_model.joblib')
+    return model
+
+@pytest.fixture(scope='module')
+def load_data():
+    data = pd.read_csv('df_dash_10.csv')
+    return data
 
 
 # Test si l'API retourne une réponse valide pour un id_client donné
 def test_api_response():
     id_client = 392266
     
-    #https://fast-api-dashboard-final.onrender.com/docs#/default/predict_predict__id_client__get
+    #https://streamlit-app-p7.onrender.com
     
     #response = requests.get(f"http://localhost:8000/predict/{id_client}")
     
-    response = requests.get(f"https://fast-api-dashboard-final.onrender.com/{id_client}")
+    response = requests.get(f"https://streamlit-app-p7.onrender.com/{id_client}")
     assert response.status_code == 200
     assert response.json()["id_client"] == id_client
 
@@ -21,17 +34,17 @@ def test_get_prediction():
     proba = get_prediction(id_client)
     assert proba >= 0 and proba <= 1
 
-# # Test si la page affiche les informations du client sélectionné
-# def test_affichage_infos_client():
-#     id_client = 392266
-#     data_client = df.loc[df["SK_ID_CURR"] == id_client]
-#     assert data_client is not None
+# Test si la page affiche les informations du client sélectionné
+def test_affichage_infos_client():
+    id_client = 392266
+    data_client = load_data.loc[load_data["SK_ID_CURR"] == id_client]
+    assert data_client is not None
 
-# # Test si la prédiction et la probabilité affichées sont valides
-# def test_prediction_proba():
-#     id_client = 392266
-#     data_client = df.loc[df["SK_ID_CURR"] == id_client]
-#     X = data_client.drop(["SK_ID_CURR", "TARGET", "prediction", "proba"], axis=1).values
-#     prediction = model.predict(X)
-#     proba = model.predict_proba(X)[:, 1][0]
-#     assert (prediction == 0 or prediction == 1) and proba >= 0 and proba <= 1
+# Test si la prédiction et la probabilité affichées sont valides
+def test_prediction_proba():
+    id_client = 392266
+    data_client = load_data.loc[load_data["SK_ID_CURR"] == id_client]
+    X = data_client.drop(["SK_ID_CURR", "TARGET", "prediction", "proba"], axis=1).values
+    prediction = load_model.predict(X)
+    proba = load_model.predict_proba(X)[:, 1][0]
+    assert (prediction == 0 or prediction == 1) and proba >= 0 and proba <= 1
