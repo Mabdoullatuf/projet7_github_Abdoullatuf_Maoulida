cc94887d804f5bca33d96e4d089763e7502531c6
Mabdoullatuf <a.ellatuf@gmail.com>
2023-07-29 01:25:24 +0200

    mise à jour des fichiers test_app.py et final_model.pickl (enrigistré avec protocol 4 car le protocol 5 qui est la derniere version n'est pas prise en compte par python 3.7

--- test_app.py
+++ test_app.py
@@ -1,44 +1,20 @@
-import joblib
-import pandas as pd
 import pytest
-import requests
-from fichier_streamlit import get_prediction
+import requests_mock
+import pandas as pd
+import numpy as np
+import os
 
+from fichier_streamlit import get_prediction  
 
-
-
-df = pd.read_csv('df_dash.csv')
-model = joblib.load('final_model.joblib')
-
-url = "http://localhost:8000/predict/"  # URL de l'API FastAPI en local
-#url = "https://fast-api-dashboard-final.onrender.com/
-# URL de l'API FastAPI en ligne
-
-
-
-
-
-def test_api():
-    id_client = 425479  # Sélectionner un identifiant client
-
-    response = requests.get(url + str(id_client))
-    assert response.status_code == 200  # Vérifier le code de statut HTTP
-
-    response_json = response.json()
-    print(response_json)
-    #assert response_json["id_client"] == id_client  # Vérifier l'identifiant client dans la réponse
-    assert 0 <= response_json["probaClasse0"] or response_json["probaClasse0"]<= 1  # Vérifier la plage de la probabilité
-    #assert response_json["prediction"] == 0 or response_json["prediction"] == 1
-    
-# def test_get_prediction():
-#     # Tester la fonction de prédiction avec un exemple d'entrée
-#     id_client = 425479
-#     prediction = get_prediction(id_client)
-#     assert isinstance(prediction, float)  # Vérifier que la prédiction est un nombre à virgule flottante
-#     assert prediction >= 0.0 and prediction <= 1.0  # Vérifier que la prédiction est dans la plage attendue
-    
-pytest.main()
-
-
+def test_get_prediction():
+    with requests_mock.Mocker() as m:
+        m.get('https://fast-api-dashboard-final.onrender.com/predict/123', json={'probaClasse0': 0.3, 'probaClasse1': 0.7})
+        prediction_data = get_prediction(123)
+        assert prediction_data['probaClasse0'] == 0.3
+        assert prediction_data['probaClasse1'] == 0.7
+        
+def test_get_prediction_response():
+    response = requests.get("https://fast-api-dashboard-final.onrender.com/predict/123")
+    assert response.status_code == 200
 
 
